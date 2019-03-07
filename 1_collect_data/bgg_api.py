db1835d0cd79b6c9a83f006153a45c8a732d2d34
steuben <32399085+kmsteuben@users.noreply.github.com>
2019-03-06 16:53:31 -0800

    pull top x00 games from bgg into dataframe

--- 1_collect_data/bgg_api.py
+++ 1_collect_data/bgg_api.py
@@ -19,3 +19,23 @@ dir(g)
 # see https://boardgamegeek.com/browse/boardgame for list of top 100 board games
 # can use this list to get rank:name and then use above to get info on that game
 
+import pandas as pd
+
+hot_games = pd.read_html("https://boardgamegeek.com/browse/boardgame")[3]
+top100 = pd.read_html("https://boardgamegeek.com/browse/boardgame", header = 0)[5]
+list(top100) # gets column names
+
+print(tables[3]) #gives hottest games
+
+# Can do same for these pages to get more than top 100
+# https://boardgamegeek.com/browse/boardgame/page/2
+top200 = pd.read_html("https://boardgamegeek.com/browse/boardgame/page/2", header = 0)[5]
+
+# Function to create dataframe of top x00 games
+def create_top_x00(x):
+	top_games = pd.read_html("https://boardgamegeek.com/browse/boardgame", header = 0)[5]
+	for page in range(2,x+1):
+	    temp = pd.read_html("https://boardgamegeek.com/browse/boardgame/page/" + str(page), header = 0)[5]
+        top_games = pd.concat([top_games, temp])
+		len(top_games)
+	return top_games
